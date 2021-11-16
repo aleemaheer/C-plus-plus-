6e785fea875e81207efd626044666571d87c28b9
aleem aheer <aleemaheer5769644@gmail.com>
2021-11-16 21:51:49 +0500

    Updated swap program

--- C++/swap.cpp
+++ C++/swap.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+// This program will get two numbers, swap their values and print the numbers after and before
+// swaping the values
 int main() {
     int a, b, c;
     std::cout << "Enter num1: ";
     std::cin >> a;
     std::cout << "Enter num2: ";
     std::cin >> b;
+    std::cout << "Before swaping the values :)\n";
+    std::cout << "Num1: " << a << "\n";
+    std::cout << "Num2: " << b << "\n";
     c = a;
     a = b;
     b = c;
+    std::cout << "After swaping the values :)\n";
     std::cout << "Num1: " << a << "\n";
     std::cout << "Num2: " << b << "\n";
 }
