be45768517f3e2df5c27e766097a8ee18a275728
aleem aheer <aleemaheer5769644@gmail.com>
2021-11-24 22:05:31 +0500

    temperature converter updated

--- C++/Temperature_converters/f_to_c.cpp
+++ C++/Temperature_converters/f_to_c.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 // This program will get temperature in fahrenheit, convert it into celcius and print their result.
+
+using namespace std;
+
 int main(void) {
 float fahrenheit, celcius;
-std:: cout << "Enter temperature in Fahrenheit: ";
-std:: cin >> fahrenheit;
+cout << "Enter temperature in Fahrenheit: ";
+cin >> fahrenheit;
 celcius = 5 / 9 * (fahrenheit - 32);
-std:: cout << "Your temperature in Celsius is: " << celcius << "\n";
+cout << "Your temperature in Celsius is: " << celcius << "\n";
 
 }
