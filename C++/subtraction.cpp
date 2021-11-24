a1ec53e8ad56df99273e46b3f6c400b059018fff
aleem aheer <aleemaheer5769644@gmail.com>
2021-11-24 22:00:49 +0500

    subtraction added

--- C++/subtraction.cpp
+++ C++/subtraction.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 
 //This program will get two numbers, subtract them and print their results.
+
+using namespace std;
+
 int main(void) {
 // Declaring variables
 int num1, num2, sum;
-std:: cout << "Enter num1: ";
-std:: cin >> num1;
-std:: cout << "Enter num2: ";
-std:: cin >> num2;
+cout << "Enter num1: ";
+cin >> num1;
+cout << "Enter num2: ";
+cin >> num2;
 sum = num1 - num2;
-std:: cout << "Subraction of num1 and num2 is: " << sum << "\n";
+cout << "Subraction of num1 and num2 is: " << sum << "\n";
 
 
 
