ad29a2a5570982ca6de95142f9db4c02af22af1a
aleem aheer <aleemaheer5769644@gmail.com>
2021-11-24 21:47:30 +0500

    even_odd updated

--- C++/even_odd.cpp
+++ C++/even_odd.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 // This program will get a number from user and check that it's even or odd 
 // and print that the number is even or odd.
+
+using namespace std;
+
 int main() {
     int num;
-    std::cout << "Enter any number: ";
-    std::cin >> num;
+    cout << "Enter any number: ";
+    cin >> num;
     if (num % 2 == 0) {
-        std::cout << "The number you entered is even.\n";
+        cout << "The number you entered is even.\n";
     }
     else {
-        std::cout << "The number you entered is odd.\n";
+        cout << "The number you entered is odd.\n";
     }
 }
