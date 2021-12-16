2c71e4ff31851b5ad79329c15403522d7b212da5
aleem aheer <aleemaheer5769644@gmail.com>
2021-12-16 21:27:13 +0500

    Updated prime number program

--- C++/prime.cpp
+++ C++/prime.cpp
@@ -1,4 +1,4 @@
-// program to check that the number is prime or not
+// Program to check that the number is prime or not
 
 #include <iostream>
 
@@ -9,14 +9,14 @@ int main () {
     int flag = 0;
     cout << "Enter a number: ";
     cin >> n;
-    for (i = (n-1); i > 1; i--) {
-        if (!(n%i) == 0) {
+    for (i = 2; i <= (n-1); i++) {
+        if ( n % i == 0) {
             flag = 1;
         }
     }
     if (flag == 1) {
-        cout << "Prime number\n";
-    } else {
         cout << "Not a prime number\n";
+    } else {
+        cout << "Prime number\n";
     }
 }
