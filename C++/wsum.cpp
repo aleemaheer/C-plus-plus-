e67a9e82927983d1e804118074aed75600bdd70d
aleem aheer <aleemaheer5769644@gmail.com>
2021-12-20 22:25:56 +0500

    Added addition program using while loop

---
+++ C++/wsum.cpp
@@ -0,0 +1,10 @@
+// This program will get two numbers from the user and add them until the 
+// users presses the 0 button
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Uncompleted
+}
