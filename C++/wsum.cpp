929906d58b58ee4329ee70f669385fff8c5e447f
aleem aheer <aleemaheer5769644@gmail.com>
2021-12-22 22:09:55 +0500

    Added a new program using while loop

--- C++/wsum.cpp
+++ C++/wsum.cpp
@@ -6,5 +6,17 @@
 using namespace std;
 
 int main() {
-    // Uncompleted
+    int num1, num2, sum;
+    cout << "Enter a first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    while (num1 != 0 && num2 != 0) {
+        sum = num1 + num2;
+        cout << "Sum of " << num1 << " and " << num2 << " is " << sum << "\n";
+        cout << "Enter a first number: ";
+        cin >> num1;
+        cout << "Enter second number: ";
+        cin >> num2;
+    }
 }
