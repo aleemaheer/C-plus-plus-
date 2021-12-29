43d5bcd0b0986e8817c80beefcb370b028d79504
aleem aheer <aleemaheer5769644@gmail.com>
2021-12-29 20:29:15 +0500

    Nested loops progam added

---
+++ C++/nested_loop/first.cpp
@@ -0,0 +1,15 @@
+// This program will print a numbers pattern.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i, j;
+    for (i = 5; i >= 1; i--) {
+        for (j = i; j >= 1; j--) {
+            cout << j;
+        }
+        cout << "\n";
+    }
+}
