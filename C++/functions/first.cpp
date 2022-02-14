115a0a9a6184a19df67e4d4268b9c2a1b5cfe3a8
aleem aheer <aleemaheer5769644@gmail.com>
2022-02-14 23:22:49 +0500

    Added division function

--- C++/functions/first.cpp
+++ C++/functions/first.cpp
@@ -10,9 +10,29 @@ int sum() {
     return sum;
 }
 
+int division() {
+    int a, b, answer;
+    cout << "Enter two numbers to divide them: \n";
+    cin >> a >> b;
+    answer = a / b;
+    return answer;
+}
+
 int main() {
     // Calling sum function in main
-    int s;
-    s = sum();
-    cout << "Sum of numbers: " << s << "\n";
+    // Asking user to choose whether he wants to perform addition or multiplication
+    int ask;
+    cout << "What you want to do multiplication or division: (1 for addition, 2 for division):\n";
+    cin >> ask;
+    if (ask == 1) {
+        int s;
+        s = sum();
+        cout << "Sum of numbers: " << s << "\n";
+    } else if (ask == 2) {
+        int s;
+        s = division();
+        cout << "Division of numbers: " << s << "\n";
+    } else {
+        cout << "Invalid choice!\n";
+    }
 }
