84e7a5b1ed5b8f47eeb0fbbbf2991ae8194afcf3
aleem aheer <aleemaheer5769644@gmail.com>
2022-02-19 21:38:34 +0500

    Added a program in basic algorithm

--- C++/basic_algorithm_practice/first.cpp
+++ C++/basic_algorithm_practice/first.cpp
@@ -3,3 +3,19 @@
 // triple means that if a user input the same two numbers for example
 // 2, 2 => 2 + 2 = 4 * 3 = 12
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, result;
+    cout << "Enter two numbers to add: \n";
+    cin >> num1 >> num2;
+    if (num1 == num2) {
+        result = (num1 + num2) * 3;
+        cout << result << "\n";
+    } else {
+        result = num1 + num2;
+        cout << result << "\n";
+    }
+}
