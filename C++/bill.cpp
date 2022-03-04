cc40564597502299cd54529413da6118debc9217
aleem aheer <aleemaheer5769644@gmail.com>
2022-03-04 23:14:26 +0500

    Done program of bill

--- C++/bill.cpp
+++ C++/bill.cpp
@@ -7,3 +7,27 @@
 // Next 100 unit will be 5.50Rs per unit
 // And add 15% tax if bill is greater than or equl to 2000
 // Or if bill is greater than equal to 5000 than add 30% tax in the bill
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int units;
+    float bill;
+    cout << "Enter units: ";
+    cin >> units;
+    if (units <= 50)
+    {
+        bill = units * 3.50;
+    }
+    if (units > 50 && units <= 150) {
+        bill = (50 * 3.50) + (units - 50) * 4.50;
+    }
+    if (units > 150) {
+        bill = (50 * 3.50) + (100 * 4.50) + (units - 150) * 5.50;
+    }
+
+    // Printing the bill
+    cout << "Your bill is " << bill;
+}
