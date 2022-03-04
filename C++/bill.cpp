593ca507cebd0c9500d4cad87daf2cef1ab609db
aleem aheer <aleemaheer5769644@gmail.com>
2022-03-04 22:56:51 +0500

    Added a program to calculate the electricity bill

---
+++ C++/bill.cpp
@@ -0,0 +1,9 @@
+// Program to calculate the bill of electricity
+// The program will take number of units from the user and prints the bill
+// in rupees
+// Bill criteria
+// first 50 units will be 3.50Rs per unit
+// Next 100 units will be 4.50Rs per unit
+// Next 100 unit will be 5.50Rs per unit
+// And add 15% tax if bill is greater than or equl to 2000
+// Or if bill is greater than equal to 5000 than add 30% tax in the bill
