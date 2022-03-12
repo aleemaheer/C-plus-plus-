ee30b6efafac897e06879b60e7eeb8d54425956c
aleem aheer <aleemaheer5769644@gmail.com>
2022-03-13 00:46:58 +0500

    Updated search program in practice folder

--- C++/practice/search.cpp
+++ C++/practice/search.cpp
@@ -4,18 +4,28 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int a[5] = {1, 2, 3, 4, 5};
-    int n, f = 0;
+    int n;
+    int i;
     cout << "Enter a number to search: ";
     cin >> n;
-    for (int i = 0; i <= 4; i++) {
-        if (n == a[i]){
+    for (i = 0; i <= 4; i++)
+    {
+        if (n == a[i])
+        {
             cout << "The number " << n << " is founded at index " << i << endl;
-            f = 1;
             break;
         }
     }
-    if (f == 0) 
-    cout << "The number " << n << " is not found." << endl;
+    //cout << "The value of i is " << i << endl;
+    if (i <= 3) {
+        // The number is founded
+    } else
+    {
+        if (a[4] != n) {
+            cout << "Your number is not founded!\n";
+        }
+    }
 }
