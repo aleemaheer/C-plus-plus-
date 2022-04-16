eab65ad5b4c413529ddfd7a80bf2c18a0b3c5246
aleem aheer <aleemaheer5769644@gmail.com>
2022-04-16 15:37:17 +0500

    Added a folder and file, started a new tutorial of c++

---
+++ C++/cpp_crash_course/index.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//using namespace std;
+
+int main() {
+    // Data Types //
+    int age = 18;
+    float weight = 54;
+    double balance = 12345.876;
+    char gender = 'f';
+    std::string username = "aleem@gmail.com";
+    bool isSunny = true;
+    std::string colors[] = {"red", "green", "blue", "yellow"};
+    isSunny ? std::cout << "Go to the park" : std::cout << "Keep rest in home";
+    std::cout << "\n";
+}
