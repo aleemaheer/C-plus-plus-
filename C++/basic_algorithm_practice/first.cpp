b866cd59dbe7a834d4540925dcf837fddab4721e
aleem aheer <aleemaheer5769644@gmail.com>
2022-07-06 17:56:45 +0500

    Updated

--- C++/basic_algorithm_practice/first.cpp
+++ C++/basic_algorithm_practice/first.cpp
@@ -1,6 +1,6 @@
 // Write a C++ program to compute the sum of the two given integer values.
 // If the two values are the same, then return triple their sum.
-// triple means that if a user input the same two numbers for example
+// triple means that if a user input the same two numbers for example 
 // 2, 2 => 2 + 2 = 4 * 3 = 12
 
 
